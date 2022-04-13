72e2406e5c25fdc5cc24b63f3ddf9e592086c8d9
Sam Smith <rakib.bubt056@gmail.com>
2022-04-13 22:19:52 +0600

    basic practice

---
+++ even-or-odd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 4/13/2022
+ * Time: 10:17 PM
+ */
+
+$number = 54;
+
+if ($number % 2 == 0){
+    echo "{$number} is even";
+}
+else{
+    echo "{$number} is odd";
+}
---
+++ largest-of-two-number.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 4/13/2022
+ * Time: 10:14 PM
+ */
+
+$a = 75;
+$b = 34;
+
+if($a > $b){
+    $large = $a;
+}
+else{
+    $large = $b;
+}
+
+echo "Large number is {$large}";
